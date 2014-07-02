7b8e1c7fa4b484cfdec40336825749fbfe743e6f
haidangbayern <haidangbayern@yahoo.com.vn>
2014-07-02 08:54:04 +0700

    config

--- www/js/config/constants.js
+++ www/js/config/constants.js
@@ -1,4 +1,4 @@
-window.server_ip = "192.168.1.4";
+window.server_ip = "192.168.3.7";
 //window.server_ip = "192.168.2.102";
 window.server_post = "8000";
 window.server_url = "http://" + window.server_ip;
