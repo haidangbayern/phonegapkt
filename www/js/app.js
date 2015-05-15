7eadb607a3e8f6b67b8e0186b535a2d52b6a2baf
haidangbayern <haidangbayern@yahoo.com.vn>
2015-05-15 13:52:25 +0700

    fix app

--- www/js/app.js
+++ www/js/app.js
@@ -316,45 +316,6 @@ angular.module('kootoro', ['ionic', 'starter.controllers']).run(function($ionicP
     {
         return obj_smileys.convert_character_to_smileys(input);
     };
-}).directive('input', function($timeout, $ionicScrollDelegate){
-    return {
-        restrict: 'E',
-        scope: {
-            'returnClose': '=',
-            'onReturn': '&',
-            'onFocus': '&',
-            'onBlur': '&'
-        },
-        link: function(scope, element, attr){
-            element.bind('focus', function(e){
-                if(scope.onFocus){
-                    $timeout(function(){
-                        scope.onFocus();
-                    });
-                    $timeout(function() {
-                    }, 200);
-
-                }        
-            });
-            element.bind('blur', function(e){
-                if(scope.onBlur){
-                    $timeout(function(){
-                        scope.onBlur();
-                    });
-                }
-            });
-            element.bind('keydown', function(e){
-                if(e.which == 13){
-                    if(scope.returnClose) element[0].blur();
-                    if(scope.onReturn){
-                        $timeout(function(){
-                            scope.onReturn();
-                        });                        
-                    }
-                } 
-            });   
-        }
-    }
 }).directive('ngEnter', function() {
     return function(scope, element, attrs) {
         element.bind("keydown keypress", function(event) {
